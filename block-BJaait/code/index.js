4fa4875c8f685ee620a608ec733338e620c9be3e
me-neeraj135 <me.neeraj1455@gmail.com>
2022-06-10 11:41:00 +0530

    drag-drop

--- block-BJaait/code/index.js
+++ block-BJaait/code/index.js
@@ -1,50 +1,81 @@
-let form = document.querySelector(`form`);
-let ul = document.querySelector(`.root`);
-let dataCards = [];
-form.addEventListener(`submit`, e => {
+var btn = document.querySelector(".add");
+var remove = document.querySelector(".draggable");
+
+function dragStart(e) {
+  this.style.opacity = "0.4";
+  // console.log(`start`);
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/html", this.innerHTML);
+}
+
+function dragEnter(e) {
+  // console.log(`enter`);
+
+  this.classList.add("over");
+}
+
+function dragLeave(e) {
+  // console.log(`leave`);
+
+  e.stopPropagation();
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  // console.log(`over`);
+
   e.preventDefault();
-  let inputValue = e.target.elements.items.value;
-  console.log(inputValue);
-  createUI(inputValue);
+  e.dataTransfer.dropEffect = "move";
+  return false;
+}
 
-  function createUI(data = dataCards) {
-    innerHTML = ``;
-    let li = document.createElement(`li`);
-    li.setAttribute(`draggable`, `true`);
-    let p = document.createElement(`p`);
-    p.innerHTML = inputValue;
-    let span = document.createElement(`span`);
-    span.innerHTML = `drag me`;
-
-    li.append(p, span);
-    ul.append(li);
-
-    li.addEventListener(`dragstart`, e => {
-      console.log(`drag-start`);
-
-      e.dataTransfer;
-    });
-
-    li.addEventListener(`dra`, e => {
-      console.log(`drag`);
-    });
-    li.addEventListener(`dragenter`, e => {
-      console.log(`drag -enter`);
-    });
-    li.addEventListener(`dragleave`, e => {
-      console.log(`drag-leave`);
-    });
-    li.addEventListener(`dragover`, e => {
-      console.log(`drag -over`);
-    });
-    li.addEventListener(`drop`, e => {
-      e.preventDefault();
-      console.log(`drop`);
-      // e.target.item.append(e.target);
-    });
-
-    li.addEventListener(`dragend`, e => {
-      console.log(`drag- end`);
-    });
+function dragDrop(e) {
+  // console.log(this);
+  if (dragSrcEl != this) {
+    // console.log(dragSrcEl);
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData("text/html");
   }
+  return false;
+}
+
+function dragEnd(e) {
+  var listItems = document.querySelectorAll(".draggable");
+  [].forEach.call(listItems, function (item) {
+    item.classList.remove("over");
+  });
+  this.style.opacity = "1";
+}
+
+function addEventsDragAndDrop(el) {
+  el.addEventListener("dragstart", dragStart, false);
+  el.addEventListener("dragenter", dragEnter, false);
+  el.addEventListener("dragover", dragOver, false);
+  el.addEventListener("dragleave", dragLeave, false);
+  el.addEventListener("drop", dragDrop, false);
+  el.addEventListener("dragend", dragEnd, false);
+}
+
+var listItems = document.querySelectorAll(".draggable");
+[].forEach.call(listItems, function (item) {
+  addEventsDragAndDrop(item);
 });
+
+function addNewItem() {
+  var newItem = document.querySelector(".input").value;
+  if (newItem != "") {
+    document.querySelector(".input").value = "";
+    var li = document.createElement("li");
+    var attr = document.createAttribute("draggable");
+    var ul = document.querySelector("ul");
+    li.className = "draggable";
+    attr.value = "true";
+    li.setAttributeNode(attr);
+    li.appendChild(document.createTextNode(newItem));
+    ul.appendChild(li);
+    addEventsDragAndDrop(li);
+  }
+}
+
+btn.addEventListener("click", addNewItem);
